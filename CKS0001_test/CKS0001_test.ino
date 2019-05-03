7c2f36c1b1856af7d869a92417fd865320c9c62c
Cokoino <37071399+Cokoino@users.noreply.github.com>
2019-05-03 23:35:31 +0800

    Add files via upload

---
+++ CKS0001_test/CKS0001_test.ino
@@ -0,0 +1,20 @@
+void setup()          
+{
+pinMode(6, OUTPUT);    
+pinMode(4, OUTPUT);   
+pinMode(5, OUTPUT);   
+pinMode(2, OUTPUT);   
+}
+void loop()     
+{
+analogWrite(6,200);   
+digitalWrite(4,HIGH); 
+analogWrite(5,200);   
+digitalWrite(2,HIGH); 
+delay(2000);         
+analogWrite(6,200);    
+digitalWrite(4,LOW);   
+analogWrite(5,200);   
+digitalWrite(2,LOW);   
+delay(2000);          
+}
